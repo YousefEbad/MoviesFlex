ca3eb12237b424b0a8729c1ac91dd28ac53864ef
YousefEbad <yousefomar6464@gmail.com>
2025-02-28 19:26:38 +0200

    first commit

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: "086cfe05dd16828e37291d2f37293a38",
+  apiKey: "f5a452626cda4ba2ce56b27606b67f70",
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
